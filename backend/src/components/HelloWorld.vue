8522c72ddca9e27be035f7ea9c2130dcb880d792
Pranab Kalita <pavankalita@gmail.com>
2022-12-14 20:03:16 +0530

    Install & Setup TailwindCSS in Vue

--- backend/src/components/HelloWorld.vue
+++ backend/src/components/HelloWorld.vue
@@ -9,7 +9,7 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 class="bg-red-500">{{ msg }}</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
@@ -21,9 +21,8 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Install
