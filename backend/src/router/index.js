5af8cf190b3ddb86b72f5c7fb21359675fe7a737
Pranab Kalita <pavankalita@gmail.com>
2022-12-17 10:05:27 +0530

    Create request password reset and reset password pages

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Dashboard from "./../views/Dashboard.vue";
 import Login from "./../views/Login.vue";
+import RequestPassword from "./../views/RequestPassword.vue";
+import ResetPassword from "./../views/ResetPassword.vue";
 
 const routes = [
     {
@@ -13,6 +15,16 @@ const routes = [
         path: "/login",
         name: "login",
         component: Login,
+    },
+    {
+        path: "/request-password",
+        name: "requestPassword",
+        component: RequestPassword,
+    },
+    {
+        path: "/reset-password/:token",
+        name: "resetPassword",
+        component: ResetPassword,
     },
 ];
 
