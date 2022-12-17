f0d4fba775376b6f191062297688149e39904dca
Pranab Kalita <pavankalita@gmail.com>
2022-12-17 15:03:57 +0530

    Redirected unauthorized users to Login Page.

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -6,12 +6,16 @@ import RequestPassword from "./../views/RequestPassword.vue";
 import ResetPassword from "./../views/ResetPassword.vue";
 import Products from "./../views/Products.vue";
 import AppLayout from "./../components/AppLayout.vue";
+import store from "../store";
 
 const routes = [
     {
         path: "/app",
         name: "app",
         component: AppLayout,
+        meta: {
+            requiresAuth: true,
+        },
         children: [
             {
                 path: "dashboard",
@@ -28,16 +32,25 @@ const routes = [
     {
         path: "/login",
         name: "login",
+        meta: {
+            requiresGuest: true,
+        },
         component: Login,
     },
     {
         path: "/request-password",
         name: "requestPassword",
+        meta: {
+            requiresGuest: true,
+        },
         component: RequestPassword,
     },
     {
         path: "/reset-password/:token",
         name: "resetPassword",
+        meta: {
+            requiresGuest: true,
+        },
         component: ResetPassword,
     },
 ];
@@ -45,6 +58,16 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) {
+        next({ name: "login" });
+    } else if (to.meta.requiresGuest && store.state.user.token) {
+        next({ name: "app.dashboard" });
+    } else {
+        next();
+    }
 });
 
 export default router;
