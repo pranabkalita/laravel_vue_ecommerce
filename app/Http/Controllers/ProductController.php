708550e8f30c4ef9cacb6dca050f6fce5da38e04
Pranab Kalita <pavankalita@gmail.com>
2022-12-19 21:09:22 +0530

    Implemented Product Create feature from Vue App

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ProductRequest;
-use App\Http\Resources\ProductListResource;
-use App\Http\Resources\ProductResource;
 use App\Models\Product;
+use Illuminate\Support\Str;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\URL;
+use App\Http\Requests\ProductRequest;
+use App\Http\Resources\ProductResource;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Resources\ProductListResource;
 
 class ProductController extends Controller
 {
@@ -29,7 +33,19 @@ class ProductController extends Controller
 
     public function store(ProductRequest $request)
     {
-        $product = Product::create($request->validated());
+        $data = $request->validated();
+        $data['created_by'] = auth()->id();
+        $data['updated_by'] = auth()->id();
+
+        $image = $data['image'] ?? null;
+        if ($image) {
+            $relativePath = $this->saveImage($image);
+            $data['image'] = URL::to(Storage::url($relativePath));
+            $data['image_mime'] = $image->getClientMimeType();
+            $data['image_size'] = $image->getSize();
+        }
+
+        $product = Product::create($data);
 
         return new ProductResource($product);
     }
@@ -52,5 +68,19 @@ class ProductController extends Controller
         $product->delete();
 
         return response()->noContent();
+    }
+
+    private function saveImage(UploadedFile $image)
+    {
+        $path = 'images/' . Str::random();
+        if (!Storage::path($path)) {
+            Storage::makeDirectory($path, 8755, true);
+        }
+
+        if (!Storage::putFileAs('public/' . $path, $image, $image->getClientOriginalName())) {
+            throw new \Exception("Unable to save file \"{$image->getClientOriginalName()}\"");
+        }
+
+        return $path . '/' . $image->getClientOriginalName();
     }
 }
--- backend/src/store/actions.js
+++ backend/src/store/actions.js
@@ -1,5 +1,35 @@
 import axiosClient from "./../axios";
 
+export async function updateProduct({ commit }, product) {
+    const id = product.id;
+    if (product.image instanceof File) {
+        const form = new FormData();
+        form.append("id", product.id);
+        form.append("title", product.title);
+        form.append("image", product.image);
+        form.append("description", product.description);
+        form.append("price", product.price);
+        // form.append("_method", 'PUT');
+
+        product = form;
+    }
+
+    return axiosClient.put(`/products/${id}`, product);
+}
+
+export async function createProduct({ commit }, product) {
+    if (product.image instanceof File) {
+        const form = new FormData();
+        form.append("title", product.title);
+        form.append("image", product.image);
+        form.append("description", product.description);
+        form.append("price", product.price);
+        product = form;
+    }
+
+    return axiosClient.post("/products", product);
+}
+
 export async function getProducts(
     { commit },
     { url = null, search = "", perPage = 10, sortField, sortDirection }
