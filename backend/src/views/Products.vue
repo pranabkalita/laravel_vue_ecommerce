c2ae152a8b52665c352f53b1d598ccdf9ac0129f
Pranab Kalita <pavankalita@gmail.com>
2022-12-19 09:15:12 +0530

    Created Product List Page Template

--- backend/src/views/Products.vue
+++ backend/src/views/Products.vue
@@ -1,7 +1,53 @@
 <template>
-    <h1>Products</h1>
+    <div class="flex items-center justify-between mb-3">
+        <h1 class="text-3xl font-semibold">Products</h1>
+        <button type="submit"
+            class="flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">Add
+            new Product</button>
+    </div>
+    <div class="bg-white p-4 rounded-lg shadow">
+        <div class="flex justify-between border-b-2 pb-3">
+            <div class="flex items-center">
+                <span class="whitespace-nowrap mr-3">Per Page</span>
+                <select
+                    class="appearance-none relative block w-24 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm">
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                    <option value="100">100</option>
+                </select>
+            </div>
+            <div>
+                <input
+                    class="appearance-none relative block w-48 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Type to Search products">
+            </div>
+        </div>
+        <Spinner v-if="false" />
+        <template v-else>
+            <table class="table-auto w-full">
+                <thead>
+                    <tr>
+                        <th class="border-b-2 p-2 text-left">ID</th>
+                        <th class="border-b-2 p-2 text-left">Image</th>
+                        <th class="border-b-2 p-2 text-left">Title</th>
+                        <th class="border-b-2 p-2 text-left">Price</th>
+                        <th class="border-b-2 p-2 text-left">Last Updated At</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <td class="border-b-2 p-2">ID</td>
+                    <td class="border-b-2 p-2">Image</td>
+                    <td class="border-b-2 p-2 max-w-[200px] whitespace-nowrap overflow-hidden text-ellipsis">Title</td>
+                    <td class="border-b-2 p-2">Price</td>
+                    <td class="border-b-2 p-2">Last Updated At</td>
+                </tbody>
+            </table>
+        </template>
+    </div>
 </template>
 
 <script setup>
-
+import Spinner from './../components/core/Spinner.vue'
 </script>
